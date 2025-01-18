122fe78a0d36391ebbfbdbe3566ef077eb8effbd
AbdulWajid768 <abdul45.wajid@gmail.com>
2025-01-19 00:47:21 +0500

    init: initial commit

--- accounts/api/v1/urls.py
+++ accounts/api/v1/urls.py
@@ -1,13 +1,12 @@
 """URLs for accounts v1 apis."""
-from dj_rest_auth.registration.views import RegisterView
-from dj_rest_auth.views import LoginView
-from django.urls import path, include
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from accounts.api.v1 import views
-
 
 urlpatterns = [
     path('signup/', views.SignupAPIView.as_view(), name='signup_api'),
     path('login/', views.LoginAPIView.as_view(), name='login_api'),
+    path('refresh-token/', TokenRefreshView.as_view(), name='token_refresh_api'),
     path('login/google/', views.GoogleLoginAPIView.as_view(), name='google_login_api'),
 ]
